3ff404a1224f3fe3f131773507ee998ac75e01ea
fikrinazilul1690 <fikrinazilul@gmail.com>
2022-08-22 14:41:54 +0700

    create openapi swagger doc

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,22 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 import { ValidationError, ValidationPipe } from '@nestjs/common';
 import { ValidationException, ValidationFilter } from './validation.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({ logger: false }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Belajar Nest Js')
+    .setDescription('Membuat API sederhana dengan nest js')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+
   app.useGlobalFilters(new ValidationFilter());
   app.useGlobalPipes(
     new ValidationPipe({
